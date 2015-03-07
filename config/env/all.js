fd4b5caf918e5dce8cc75f94ef39d01395f750e9
Praseetha-KR <voidimagineer@yahoo.com>
2015-03-07 21:27:03 +0530

    added masonry

--- config/env/all.js
+++ config/env/all.js
@@ -47,12 +47,24 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
 			],
 			js: [
+				'public/lib/jquery/dist/jquery.min.js',
 				'public/lib/angular/angular.js',
 				'public/lib/angular-resource/angular-resource.js',
 				'public/lib/angular-animate/angular-animate.js',
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/eventie/eventie.js',
+				'public/lib/doc-ready/doc-ready.js',
+				'public/lib/eventEmitter/eventEmitter.js',
+				'public/lib/get-style-property/get-style-property.js',
+				'public/lib/get-size/get-size.js',
+				'public/lib/matches-selector/matches-selector.js',
+				'public/lib/outlayer/item.js',
+				'public/lib/outlayer/outlayer.js',
+				'public/lib/masonry/dist/masonry.pkgd.js',
+				'public/lib/imagesloaded/imagesloaded.pkgd.js',
+				'public/lib/angular-masonry/angular-masonry.js'
 			]
 		},
 		css: [
--- public/config.js
+++ public/config.js
@@ -4,7 +4,8 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'papercuts';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngAnimate', 'ui.router', 'ui.bootstrap', 
+												'ui.utils', 'wu.masonry'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
